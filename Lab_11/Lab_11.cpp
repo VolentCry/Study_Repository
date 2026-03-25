8b6d348810d9322181f956607d3d304e28990791
Simon <tokgrom84@gmail.com>
2026-03-25 10:26:22 +0500

    Add lab №11

--- Lab_11/Lab_11.cpp
+++ Lab_11/Lab_11.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 struct Node {
-	int data;
+	string data;
 	Node* pointer_to_prev_node = nullptr;
 	Node* pointer_to_next_node = nullptr;
 };
@@ -13,8 +14,17 @@ struct List {
 	Node* tail_node = nullptr;
 };
 
+void PrintList(List list) {
+	Node* cur = list.head_node;
+	while (cur != nullptr) {
+		cout << cur->data << ' ';
+		cur = cur->pointer_to_next_node;
+	}
+	delete cur;
+	cout << endl;
+}
 
-void InsertItem(List& list, const int& data, const int& index = 0) {
+void InsertItem(List& list, const string& data, const int& index = 0) {
 	Node* new_node = new Node;
 	new_node->data = data;
 
@@ -59,9 +69,49 @@ void InsertItem(List& list, const int& data, const int& index = 0) {
 
 	current_node->pointer_to_next_node = new_node;
 }
+
+void DeleteElement(List& list, const string& del_str) {
+	Node* current = list.head_node;
+	while (current != nullptr) {
+		if (current->data == del_str) {
+			Node* toDelete = current; // Сохраняем удаляемый узел
+			current = current->pointer_to_next_node; // Переходим к следующему до удаления
+
+			if (toDelete == list.head_node && toDelete == list.tail_node) {
+				// Единственный элемент
+				list.head_node = nullptr;
+				list.tail_node = nullptr;
+			} else if (toDelete == list.head_node) {
+				// Удаление "головы" списка
+				list.head_node = toDelete->pointer_to_next_node;
+				list.head_node->pointer_to_prev_node = nullptr;
+			} else if (toDelete == list.tail_node) {
+				// Удаление "хвоста" списка
+				list.tail_node = toDelete->pointer_to_prev_node;
+				list.tail_node->pointer_to_next_node = nullptr;
+			} else {
+				// Удаление из середины списка
+				toDelete->pointer_to_prev_node->pointer_to_next_node = toDelete->pointer_to_next_node;
+				toDelete->pointer_to_next_node->pointer_to_prev_node = toDelete->pointer_to_prev_node;
+			}
+
+			delete toDelete; // Удаление сохранённого узла
+		} else {
+			current = current->pointer_to_next_node;
+		}
+	}
+
+}
 	
 int main() {
-	List list;
-	
+	List test_list;
+	int N = 5;
+	cout << "\n";
+	InsertItem(test_list, "FFF", 0);
+	InsertItem(test_list, "DDD", 1);
+	InsertItem(test_list, "SSS", 2);
+	PrintList(test_list);
+	DeleteElement(test_list, "DDD");
+	PrintList(test_list);
 	return 0;
 }
