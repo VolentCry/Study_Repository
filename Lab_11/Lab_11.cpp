4f74754430a82d12e21f1df58179b79fa03b7693
DarkScara <tokgrom84@gmail.com>
2026-03-26 15:37:29 +0300

    Edit Lab 11

--- Lab_11/Lab_11.cpp
+++ Lab_11/Lab_11.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<clocale>
 using namespace std;
 
 struct Node {
@@ -14,6 +15,8 @@ struct List {
 	Node* tail_node = nullptr;
 };
 
+
+
 void PrintList(List list) {
 	Node* cur = list.head_node;
 	while (cur != nullptr) {
@@ -22,6 +25,29 @@ void PrintList(List list) {
 	}
 	delete cur;
 	cout << endl;
+}
+
+void DestroyList(List& list) {
+	Node* current = list.head_node;
+	while (current != nullptr) {
+		Node* next = current->pointer_to_next_node;
+		delete current;
+		current = next;
+	}
+
+	list.head_node = nullptr;
+	list.tail_node = nullptr;
+}
+
+int ListLen(List list) {
+	Node* cur = list.head_node;
+	int lenght = 0;
+	while (cur != nullptr) {
+		lenght++;
+		cur = cur->pointer_to_next_node;
+	}
+	delete cur;
+	return lenght;
 }
 
 void InsertItem(List& list, const string& data, const int& index = 0) {
@@ -59,7 +85,7 @@ void InsertItem(List& list, const string& data, const int& index = 0) {
 
 	new_node->pointer_to_prev_node = current_node;
 	new_node->pointer_to_next_node = current_node->pointer_to_next_node;
-	
+
 	if (current_node->pointer_to_next_node != nullptr) {
 		current_node->pointer_to_next_node->pointer_to_prev_node = new_node;
 	}
@@ -70,7 +96,7 @@ void InsertItem(List& list, const string& data, const int& index = 0) {
 	current_node->pointer_to_next_node = new_node;
 }
 
-void DeleteElement(List& list, const string& del_str) {
+void DeleteElementByKey(List& list, const string& del_str) {
 	Node* current = list.head_node;
 	while (current != nullptr) {
 		if (current->data == del_str) {
@@ -81,37 +107,69 @@ void DeleteElement(List& list, const string& del_str) {
 				// Единственный элемент
 				list.head_node = nullptr;
 				list.tail_node = nullptr;
-			} else if (toDelete == list.head_node) {
+			}
+			else if (toDelete == list.head_node) {
 				// Удаление "головы" списка
 				list.head_node = toDelete->pointer_to_next_node;
 				list.head_node->pointer_to_prev_node = nullptr;
-			} else if (toDelete == list.tail_node) {
+			}
+			else if (toDelete == list.tail_node) {
 				// Удаление "хвоста" списка
 				list.tail_node = toDelete->pointer_to_prev_node;
 				list.tail_node->pointer_to_next_node = nullptr;
-			} else {
+			}
+			else {
 				// Удаление из середины списка
 				toDelete->pointer_to_prev_node->pointer_to_next_node = toDelete->pointer_to_next_node;
 				toDelete->pointer_to_next_node->pointer_to_prev_node = toDelete->pointer_to_prev_node;
 			}
 
 			delete toDelete; // Удаление сохранённого узла
-		} else {
+		}
+		else {
 			current = current->pointer_to_next_node;
 		}
 	}
 
 }
-	
+
+
 int main() {
 	List test_list;
-	int N = 5;
-	cout << "\n";
-	InsertItem(test_list, "FFF", 0);
-	InsertItem(test_list, "DDD", 1);
-	InsertItem(test_list, "SSS", 2);
+	int K;
+	setlocale(LC_ALL, "ru_RU.UTF-8");
+
+	// Заполнение списка элементами для демонстрации
+	InsertItem(test_list, "Apple", 0);
+	InsertItem(test_list, "Orange", 1);
+	InsertItem(test_list, "Banana", 2);
+
+	// Печать списка
 	PrintList(test_list);
-	DeleteElement(test_list, "DDD");
+
+	// Удаление элемента по ключу
+	DeleteElementByKey(test_list, "Orange");
+
+	// Печать списка
 	PrintList(test_list);
+
+
+	// Добавление K элементов в конец списка
+	cout << "Введите количество элементов K: ";
+	cin >> K;
+
+	cout << "Вводите элементы:" << endl;
+	for (int i = 0; i < K; ++i) {
+		string input_data;
+		cout << "Элемент " << (i + 1) << ": ";
+		cin >> input_data;
+
+		// Добавляем элемент в конец списка
+		InsertItem(test_list, input_data, ListLen(test_list));
+	}
+
+	// Печать списка
+	PrintList(test_list);
+
 	return 0;
 }
